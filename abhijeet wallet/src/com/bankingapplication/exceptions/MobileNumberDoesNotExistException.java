c237bd21aca3a12ef9f2bec6c6ecffb48863ce03
nitishaagrawal123 <47311849+nitishaagrawal123@users.noreply.github.com>
2019-03-02 09:45:46 +0530

    Add files via upload

---
+++ abhijeet wallet/src/com/bankingapplication/exceptions/MobileNumberDoesNotExistException.java
@@ -0,0 +1,14 @@
+package com.bankingapplication.exceptions;
+@SuppressWarnings("serial")
+public class MobileNumberDoesNotExistException extends Exception {
+	
+	
+	@Override
+	public String getMessage() {
+	
+		return "Invalid Mobile Number";
+		
+	}
+
+	
+}
