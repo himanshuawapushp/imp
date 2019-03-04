7a1183be63eb197cc63970cc5327b89efc74051d
nitishaagrawal123 <47311849+nitishaagrawal123@users.noreply.github.com>
2019-03-04 17:30:57 +0530

    Add files via upload

---
+++ SpringMVCApplication/src/com/cg/springmvc/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.cg.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/homePage")
+	public String displayHomePage() {
+		return "home";
+	}
+}
